14da4e86a704336659170be48af2ad3c580e9f00
peter <pkeoman17@gmail.com>
2014-07-27 19:13:39 -0700

    pwm_lld init

---
+++ os/hal/platforms/Posix/pwm_lld.h
@@ -0,0 +1,256 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    templates/pwm_lld.h
+ * @brief   PWM Driver subsystem low level driver header template.
+ *
+ * @addtogroup PWM
+ * @{
+ */
+
+#ifndef _PWM_LLD_H_
+#define _PWM_LLD_H_
+
+#if HAL_USE_PWM || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Number of PWM channels per PWM driver.
+ */
+#define PWM_CHANNELS                        4
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+/**
+ * @name    Configuration options
+ * @{
+ */
+/**
+ * @brief   PWM driver enable switch.
+ * @details If set to @p TRUE the support for PWM1 is included.
+ */
+#if !defined(PLATFORM_PWM_USE_PWM1) || defined(__DOXYGEN__)
+#define PLATFORM_PWM_USE_PWM1               FALSE
+#endif
+/** @} */
+
+#define PWM_OUTPUT_MASK                 0x0F
+#define PWM_OUTPUT_DISABLED             0x00
+#define PWM_OUTPUT_ACTIVE_HIGH          0x01 
+#define PWM_OUTPUT_ACTIVE_LOW           0x02
+
+#define PWM_OUTPUT_LEFT                 0x00000000
+
+
+#define PWM_OUTPUT_CENTER               0x00000010
+
+
+#define PWM_MCK_DIV_1                   0x00000000
+#define PWM_MCK_DIV_2                   0x00000100
+#define PWM_MCK_DIV_4                   0x00000200
+#define PWM_MCK_DIV_8                   0x00000300
+#define PWM_MCK_DIV_16                  0x00000400
+#define PWM_MCK_DIV_32                  0x00000500
+#define PWM_MCK_DIV_64                  0x00000600
+#define PWM_MCK_DIV_128                 0x00000700
+#define PWM_MCK_DIV_256                 0x00000800
+#define PWM_MCK_DIV_512                 0x00000900
+#define PWM_MCK_DIV_1024                0x00000A00
+#define PWM_MCK_DIV_CLKA                0x00000B00
+#define PWM_MCK_DIV_CLKB                0x00000C00
+
+#define PWM_OUTPUT_PIN1                 0x00001000
+#define PWM_OUTPUT_PIN2                 0x00002000
+#define PWM_OUTPUT_PIN3                 0x00004000
+
+#define PWM_DISABLEPULLUP_PIN1          0x00010000
+#define PWM_DISABLEPULLUP_PIN2          0x00020000
+#define PWM_DISABLEPULLUP_PIN3          0x00040000
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief PWM mode type.
+ */
+typedef uint32_t pwmmode_t;
+
+/**
+ * @brief   PWM channel type.
+ */
+typedef uint8_t pwmchannel_t;
+
+/**
+ * @brief   PWM counter type.
+ */
+typedef uint16_t pwmcnt_t;
+
+typedef struct {
+  uint32_t     CR1;
+  uint32_t     CR2;
+  uint32_t     SMCR;
+  uint32_t     DIER;
+  uint32_t     SR;
+  uint32_t     EGR;
+  uint32_t     CCMR1;
+  uint32_t     CCMR2;
+  uint32_t     CCER;
+  uint32_t     CNT;
+  uint32_t     PSC;
+  uint32_t     ARR;
+  uint32_t     RCR;
+  uint32_t     CCR[4];
+  uint32_t     BDTR;
+  uint32_t     DCR;
+  uint32_t     DMAR;
+  uint32_t     OR;
+  uint32_t     CCMR3;
+  uint32_t     CCR5;
+  uint32_t     CCR6;
+} sim_t;
+/**
+ * @brief   PWM driver channel configuration structure.
+ * @note    Some architectures may not be able to support the channel mode
+ *          or the callback, in this case the fields are ignored.
+ */
+typedef struct {
+  /**
+   * @brief Channel active logic level.
+   */
+  pwmmode_t                 mode;
+  /**
+   * @brief Channel callback pointer.
+   * @note  This callback is invoked on the channel compare event. If set to
+   *        @p NULL then the callback is disabled.
+   */
+  pwmcallback_t             callback;
+  /* End of the mandatory fields.*/
+} PWMChannelConfig;
+
+/**
+ * @brief   Driver configuration structure.
+ * @note    Implementations may extend this structure to contain more,
+ *          architecture dependent, fields.
+ */
+typedef struct {
+  /**
+   * @brief   Timer clock in Hz.
+   * @note    The low level can use assertions in order to catch invalid
+   *          frequency specifications.
+   */
+  uint32_t                  frequency;
+  /**
+   * @brief   PWM period in ticks.
+   * @note    The low level can use assertions in order to catch invalid
+   *          period specifications.
+   */
+  pwmcnt_t                  period;
+  /**
+   * @brief Periodic callback pointer.
+   * @note  This callback is invoked on PWM counter reset. If set to
+   *        @p NULL then the callback is disabled.
+   */
+  pwmcallback_t             callback;
+  /**
+   * @brief Channels configurations.
+   */
+  PWMChannelConfig          channels[PWM_CHANNELS];
+  /* End of the mandatory fields.*/
+  uint32_t                  clock;
+  sim_t                      *sim;
+} PWMConfig;
+
+
+
+/**
+ * @brief   Structure representing an PWM driver.
+ * @note    Implementations may extend this structure to contain more,
+ *          architecture dependent, fields.
+ */
+struct PWMDriver {
+  /**
+   * @brief Driver state.
+   */
+  pwmstate_t                state;
+  /**
+   * @brief Current configuration data.
+   */
+  const PWMConfig           *config;
+  /**
+   * @brief   Current PWM period in ticks.
+   */
+  pwmcnt_t                  period;
+#if defined(PWM_DRIVER_EXT_FIELDS)
+  PWM_DRIVER_EXT_FIELDS
+#endif
+  /* End of the mandatory fields.*/
+
+};
+
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/**
+ * @brief   Returns a PWM channel status.
+ * @pre     The PWM unit must have been activated using @p pwmStart().
+ *
+ * @param[in] pwmp      pointer to a @p PWMDriver object
+ * @param[in] channel   PWM channel identifier (0...PWM_CHANNELS-1)
+ *
+ * @notapi
+ */
+#define pwm_lld_is_channel_enabled(pwmp, channel) FALSE
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#if PLATFORM_PWM_USE_PWM1 && !defined(__DOXYGEN__)
+extern PWMDriver PWMD1;
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void pwm_lld_init(void);
+  void pwm_lld_start(PWMDriver *pwmp);
+  void pwm_lld_stop(PWMDriver *pwmp);
+  void pwm_lld_change_period(PWMDriver *pwmp, pwmcnt_t period);
+  void pwm_lld_enable_channel(PWMDriver *pwmp,
+                              pwmchannel_t channel,
+                              pwmcnt_t width);
+  void pwm_lld_disable_channel(PWMDriver *pwmp, pwmchannel_t channel);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_USE_PWM */
+
+#endif /* _PWM_LLD_H_ */
+
+/** @} */
